82457805d796cfd42ebebb2f8be070668c1f3e4c
tgavri <thom54s8@stud.kea.dk>
2024-03-10 17:47:38 +0100

    Initial commit

---
+++ src/main/java/dk/kea/gruppe5_madspild/Gruppe5MadspildApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.gruppe5_madspild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe5MadspildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gruppe5MadspildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/gruppe5_madspild/Gruppe5MadspildApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.gruppe5_madspild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppe5MadspildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
